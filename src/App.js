2853ef04cdc9396ece28f96e1cf38d724535b3c1
Rohan Mudliar <rohan.mudliar@oonsterindia.com>
2023-04-15 22:16:35 +0530

    add theme styles and topbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import TopBar from "./global/Topbar";
+
 function App() {
-  return <div className="app">Hello</div>;
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline>
+          <div className="app">
+            <main className="content">
+              <TopBar />
+            </main>
+          </div>
+        </CssBaseline>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
 export default App;
